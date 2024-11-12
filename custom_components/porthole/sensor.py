7da2b3546931e12700939c8e5e2c78763e5819ae
c3p0vsr2d2 <90661423+c3p0vsr2d2@users.noreply.github.com>
2024-11-12 15:35:33 -0800

    Update sensor.py
    Adding a switch entity

--- custom_components/porthole/sensor.py
+++ custom_components/porthole/sensor.py
@@ -1,8 +1,11 @@
 import logging
 from datetime import timedelta, datetime
 import aiohttp
-from homeassistant.components.sensor import SensorEntity
+
 from homeassistant.helpers.entity import Entity
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.components.switch import SwitchEntity
+
 from homeassistant.helpers import device_registry as dr
 from homeassistant.helpers.device_registry import DeviceEntry
 import asyncio
@@ -526,6 +529,78 @@ class PortainerContainerSensor(SensorEntity):
             "name": endpoint_info["name"],
             "manufacturer": "Portainer"
             }
+
+    async def async_update(self):
+        """Update the server's state and attributes."""
+        endpoint_info = self._portainer_obj["endpoints"][self._endpoint_index]
+        _LOGGER.debug(f"Updating Portainer Container sensor: {endpoint_info["endpoint_id"]}")
+        await self._portainer.update()
+
+class PortainerContainerSwitch(SwitchEntity):
+
+    def __init__(self, portainer, endpoint_index, container_index):
+        self._portainer = portainer
+        self._portainer_obj = self._portainer.portainer_obj
+        self._endpoint_index = endpoint_index
+        self._container_index = container_index
+        
+    @property
+    def unique_id(self):
+        """Return a unique ID for the entity, based on container name."""
+        _LOGGER.debug(self._container_index)
+        return self._portainer_obj["endpoints"][self._endpoint_index]["containers"][self._container_index]["name"]
+
+    @property
+    def name(self):
+        """Return the name of the entity."""
+        return self._portainer_obj["endpoints"][self._endpoint_index]["containers"][self._container_index]["name"]
+
+    @property
+    def state(self):
+        """Return the current state of the container (status)."""
+        # Use the "Status" field from Portainer to represent the state
+        return self._portainer_obj["endpoints"][self._endpoint_index]["containers"][self._container_index]["state"]
+
+    @property
+    def is_on(self) -> bool:
+        """Return true if the switch is on."""
+        return self._state
+
+    def turn_on(self, **kwargs) -> None:
+        """Turn the switch on."""
+        _LOGGER.info(f"Turning on the switch: {self._name}")
+        self._state = True
+
+    def turn_off(self, **kwargs) -> None:
+        """Turn the switch off."""
+        _LOGGER.info(f"Turning off the switch: {self._name}")
+        self._state = False
+        
+    @property
+    def icon(self):
+        """Return the icon to represent this container."""
+        return "mdi:docker"
+
+    @property
+    def extra_state_attributes(self):
+        """Return additional state attributes."""
+        endpoint_info = self._portainer_obj["endpoints"][self._endpoint_index]
+        container_info = endpoint_info["containers"][self._container_index]
+
+        return {
+            "Name": container_info["name"],
+            "Image": container_info["image"],
+            "ContainerId": container_info["container_id"],
+            "Created": container_info["created"],
+            "Status": container_info["status"],
+            "State": container_info["state"],
+            "Ports": container_info["ports"],  # Get formatted ports
+            "EndpointId": endpoint_info["endpoint_id"],
+            "EndpointURL": endpoint_info["endpoint_url"],
+            "EndpointName": endpoint_info["friendly_name"],
+            "PortainerId": self._portainer_obj["portainer_id"],
+            "Version": self._portainer_obj["portainer_version"],
+        }
 
     async def async_update(self):
         """Update the server's state and attributes."""
