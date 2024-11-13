68e90d46f765073702fb96cc5101c8cf338099de
c3p0vsr2d2 <90661423+c3p0vsr2d2@users.noreply.github.com>
2024-11-13 09:17:54 -0800

    Update portainer_endpoint_sensor.py

--- custom_components/porthole/sensors/portainer_endpoint_sensor.py
+++ custom_components/porthole/sensors/portainer_endpoint_sensor.py
@@ -11,7 +11,7 @@ from homeassistant.helpers.device_registry import DeviceEntry
 import asyncio
 from homeassistant.util import Throttle
 
-from porthole import PortainerServer
+from ..porthole_server import PortainerServer
 
 class PortainerEndpointSensor(SensorEntity):
     """Sensor representing the Portainer server."""
