68bd7d8a761a2bc38f06c678b2ca938d948f66f0
c3p0vsr2d2 <90661423+c3p0vsr2d2@users.noreply.github.com>
2024-11-13 09:10:07 -0800

    Update const.py

--- custom_components/porthole/const.py
+++ custom_components/porthole/const.py
@@ -1,8 +1,5 @@
 # Domain name
 DOMAIN = "porthole"
 
-# Define the minimum time between updates (e.g., 5 minutes)
-MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=5)
-
 # Define platform names as constants to avoid magic strings
 PLATFORM_SENSOR = "ingress"
