b64f6a51aea5344a1a6fe503438b39cf4c58432d
c3p0vsr2d2 <90661423+c3p0vsr2d2@users.noreply.github.com>
2024-11-15 06:39:31 -0800

    Update sensor.py

--- custom_components/porthole/sensor.py
+++ custom_components/porthole/sensor.py
@@ -26,10 +26,10 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(hass, entry, async_add_entities):
 
     """Set up Portainer from a config entry."""
-    _LOGGER.debug("Setting up Portainer integration with config entry.")
+    _LOGGER.error("Setting up Portainer integration with config entry.")        ##
 
     portainer = entry.portainer
-    
+     _LOGGER.error("1 entry.")        ##
     try:
         # Add a server sensor for the Portainer server itself
         server_sensor = PortainerServerSensor(portainer)
@@ -37,6 +37,8 @@ async def async_setup_entry(hass, entry, async_add_entities):
     except Exception as e:
         _LOGGER.error(f"Error adding Portainer Server sensor: {e}")
         return False
+
+    _LOGGER.error("1 entry.")        ##
         
     try:
         # Add endpoint sensors and container sensors for each endpoint
