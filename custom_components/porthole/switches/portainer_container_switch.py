15abce7b27a854e083525b212372d6356c6c6800
c3p0vsr2d2 <90661423+c3p0vsr2d2@users.noreply.github.com>
2024-11-15 09:52:26 -0800

    Update portainer_container_switch.py

--- custom_components/porthole/switches/portainer_container_switch.py
+++ custom_components/porthole/switches/portainer_container_switch.py
@@ -37,7 +37,7 @@ class PortainerContainerSwitch(SwitchEntity):
     def state(self):
         """Return the current state of the container (status)."""
         # Use the "Status" field from Portainer to represent the state
-        return (self._portainer_obj["endpoints"][self._endpoint_index]["containers"][self._container_index]["state"] == "running")
+        return 1 if (self._portainer_obj["endpoints"][self._endpoint_index]["containers"][self._container_index]["state"] == "running") else 0
 
     @property
     def is_on(self) -> bool:
